2491b99b0070cfeb224b0bbc63e529a800ef4092
ks-ampangkl <kugaann@graduate.utm.my>
2026-05-22 16:10:31 +0800

    search and filter

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const columns = document.getElementById("grids");
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const filter = document.getElementById("genre");
+
+let allMovies = [];
+
+//why we put render in a function? because if we dont it all renders once only, so if we filter any search, we can do it more than once iwth this function.
 
 
+function renderData(movieArray){
+    
+    columns.innerHTML = "";//clears old cards
 
-fetch("info.json")
-.then(response => response.json())
-.then( data => 
-    {data.forEach(moviesData => {
-        
+    movieArray.forEach(moviesData => {
+
         const movie = document.createElement("div");
 
         movie.classList.add("movie");
@@ -20,6 +27,38 @@ fetch("info.json")
         `;
 
 
-        columns.appendChild(movie);
-    })
+        columns.appendChild(movie);    
+    }
+    );
+
+
+}
+
+//SEARCHING//
+submit.addEventListener("click", () => {
+
+    const movie = search.value.toLowerCase();
+    const type = filter.value.toLowerCase();
+
+    const filteredMovies = allMovies.filter(movies => movies.title.toLowerCase().includes(movie) && movies.genre.toLowerCase().includes(type));
+
+    renderData(filteredMovies);
+})
+
+
+// search.addEventListener("input",() =>{
+//     const movie = search.value.toLowerCase();
+
+//     const filteredMovies = allMovies.filter(movies => movies.title.toLowerCase().includes(movie));
+//     // determining what filtered movies are based on: filter allMovies array: checking if movies has same title as movie.includes to check if it is available through case insensitive scenario.
+//     renderData(filteredMovies);
+// })
+
+
+fetch("info.json")
+.then( response => response.json())
+.then( data => {
+    
+    allMovies = data;
+    renderData(allMovies);
 });
